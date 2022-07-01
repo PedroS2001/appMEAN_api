e9d6e5ca79bdb166d6d68448fa8416b97eda43e1
Pedro <pedroseneriz01@gmail.com>
2022-07-01 16:06:43 -0300

    require dotenv and connect to mongo

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 const express = require('express');
+require('dotenv').config()
+
+const mongoose = require('mongoose');
 
 const app = express();
+
+
+mongoose.connect(process.env.MONGO_URL + process.env.MONGO_DB_NAME  )
+    .then(() => console.log('Conectado a MongoDB'))
+    .catch((err) => console.log('No se pudo conectar con mongodb' + err))
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
