7e32e4b2d3139e60a501c156d17e712b29735952
Pedro Seneriz <pedroseneriz01@gmail.com>
2022-07-13 15:56:08 -0300

    actualizo images and cors

--- src/app.js
+++ src/app.js
@@ -7,14 +7,11 @@ const cors = require('cors')
 const auth = require('./routes/auth');
 const usuarios = require('./routes/usuarios');
 
-const corsOptions = {
-    origin: "*"
-};
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.static('public'));
 
